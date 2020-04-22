956c88f08b1dc8917ec9efcc66798f5d5fdbf524
manisha shelke <manisha.shelke123@gmail.com>
2020-04-22 14:55:42 +0530

    Initial Commit

---
+++ src/test/java/testcases/LoginTest.java
@@ -0,0 +1,47 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+public class LoginTest {
+
+	public static WebDriver driver;
+
+	@BeforeSuite
+	public void setUp() {
+		
+			WebDriverManager.firefoxdriver().setup();
+			 driver=new FirefoxDriver();
+
+		
+		}
+	
+	
+	@Test
+	public void doLogin() {
+		driver.get("http://gmail.com");
+	
+		//WebDriverWait wait=new WebDriverWait(driver, 20);
+		System.out.println(driver.getTitle());
+		
+		driver.findElement(By.id("identifierId")).sendKeys("manaliprafullpihu@gmail.com");
+		
+		driver.findElement(By.xpath("//*[@id='identifierNext']/span")).click();
+		
+
+
+}
+	@AfterSuite
+	  public void tearDown() {
+			
+			driver.close();
+			System.out.println("Quitting  Evarything !!!");
+		}
+		
+
+}
